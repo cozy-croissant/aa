74fb5750109f5cd6d6f2c310352acbdee8cb3f39
cozy-croissant <90201920+cozy-croissant@users.noreply.github.com>
2021-09-18 10:08:45 +0900

    update c02

---
+++ c02/ex01/ft_strncpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hoekim <hoekim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/18 00:09:54 by hoekim            #+#    #+#             */
+/*   Updated: 2021/09/18 00:15:57 by hoekim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+		dest[i++] = 0;
+	return (dest);
+}
