266b433e4e3e359bd637f02b0cd711b53f1a7941
Hoekang Kim <hoekim@c4r4s4.42seoul.kr>
2021-09-22 21:54:50 +0900

    put_atoi_base

--- c04/ex05/ft_atoi_base.c
+++ c04/ex05/ft_atoi_base.c
@@ -6,8 +6,38 @@
 /*   By: hoekim <hoekim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 21:48:36 by hoekim            #+#    #+#             */
-/*   Updated: 2021/09/21 21:48:40 by hoekim           ###   ########.fr       */
+/*   Updated: 2021/09/22 21:52:20 by hoekim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 
+int	ft_atoi_base(char *str, char *base)
+{
+	int	i;
+	int	n;
+	int	nega;
+	int	len;
+
+	i = 0;
+	n = 0;
+	nega = 1;
+	len = ft_len_check(base);
+	if (ft_len_check(base) == 1)
+		return (0);
+	while ((9 <= str[i] && str[i] <= 13) || str[i] == 32)
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			nega *= -1;
+		i++;
+	}
+	while (s)
+	{
+		i
+		n = n * len + 
+		i++;
+	}
+	return (n * nega);
+}
