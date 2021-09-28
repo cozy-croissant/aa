bc7b8b16fe031cc068f0fafdcb81070675a9bc92
cozy-croissant <90201920+cozy-croissant@users.noreply.github.com>
2021-09-28 21:13:10 +0900

    we are the world

---
+++ c09/ex02/ft_split.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hoekim <hoekim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/27 01:22:38 by hoekim            #+#    #+#             */
+/*   Updated: 2021/09/28 21:11:21 by hoekim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ck_charset(char c, char *charset)
+{
+	while (*charset)
+	{
+		if (c == *charset)
+			return (1);
+		charset++;
+	}
+	return (0);
+}
+
+long	ft_strlen(char *str, char *charset)
+{
+	long	cnt;
+
+	cnt = 0;
+	while (*str)
+	{
+		if (!ck_charset(*str, charset))
+		{
+			str++;
+			cnt++;
+			while (*str && !ck_charset(*str, charset))
+				str++;
+			str--;
+		}
+		str++;
+	}
+	return (cnt);
+}
+
+void	ft_strcpy(char *dest, char *src, char *str)
+{
+	while (src < str)
+		*(dest)++ = *(src)++;
+	*dest = 0;
+}
+
+char	**ft_split(char *str, char *charset)
+{
+	char	**strs;
+	char	*tmp;
+	int		i;
+
+	i = 0;
+	strs = malloc(sizeof(char *) * ft_strlen(str, charset) + 1);
+	while (*str)
+	{
+		if (!ck_charset(*str, charset))
+		{
+			tmp = str;
+			str++;
+			while (!ck_charset(*str, charset) && *str)
+				++str;
+			strs[i] = malloc(str - tmp + 1);
+			ft_strcpy(strs[i++], tmp, str);
+		}
+		str++;
+	}
+	strs[i] = 0;
+	return (strs);
+}
